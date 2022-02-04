dc7cd96f91d8e56e70672d9c0bab4a7511ae6b68
Eshaan Agarwal <eshaan060202@gmail.com>
2022-02-04 19:37:02 +0530

    add inline documentation for communications/message.py (#144)
    * add inline documentation for communications/message.py
    
    * fix documentation styling
    
    * fix inline documentation for communications/message.py

--- fedrec/communications/messages.py
+++ fedrec/communications/messages.py
@@ -8,6 +8,16 @@ from dataclasses import dataclass
 @registry.load("serializer", "Message")
 @dataclass
 class Message(object):
+    '''
+    Base class that is inherited by other Message classes
+
+    Attributes:
+    -----------
+        senderid : str
+            id of sender
+        receiverid : str
+            id of receiver
+    '''
     __type__ = "Message"
 
     def __init__(self, senderid, receiverid):
@@ -15,15 +25,35 @@ class Message(object):
         self.receiverid = receiverid
 
     def get_sender_id(self):
+        '''Returns senderid from Message Object'''
         return self.senderid
 
     def get_receiver_id(self):
+        '''Returns senderid from Message Object'''
         return self.receiverid
 
 
 @registry.load("serializer", "JobSubmitMessage")
 @dataclass
 class JobSubmitMessage(Message):
+    '''
+    Creates a message object for job submit request
+
+    Attributes:
+    -----------
+        job_type : str
+            type of job
+        job_args : list
+            list of job arguments
+        job_kwargs: dict
+            Extra key-pair arguments related to job
+        senderid : str
+            id of sender
+        receiverid : str
+            id of reciever
+        workerstate : ActorState
+            ActorState object containing worker's state
+    '''
     __type__ = "JobSubmitMessage"
 
     def __init__(self,
@@ -40,15 +70,32 @@ class JobSubmitMessage(Message):
         self.workerstate: ActorState = workerstate
 
     def get_worker_state(self):
+        '''Returns workerstate from JobSubmitMessage Object'''
         return self.workerstate
 
     def get_job_type(self):
+        '''Returns job_type from JobSubmitMessage Object'''
         return self.job_type
 
 
 @registry.load("serializer", "JobResponseMessage")
 @dataclass
 class JobResponseMessage(Message):
+    '''
+    Creates message objects for job response message
+
+    Attributes:
+    -----------
+        job_type : str
+            type of job (train/test)
+        senderid : str
+            id of sender
+        receiverid : str
+            id of receiver
+        results : dict
+            dict of results obtained from job completion
+        errors : null
+    '''
     __type__ = "JobResponseMessage"
 
     def __init__(self, job_type, senderid, receiverid):
@@ -59,6 +106,10 @@ class JobResponseMessage(Message):
 
     @property
     def status(self):
+        '''
+        Check if errors is None and returns response
+        message status accordingly
+        '''
         if self.errors is None:
             return True
         else:
